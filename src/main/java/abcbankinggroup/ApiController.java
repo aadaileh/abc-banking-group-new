5076fad5f22534448930796090351993f101b646
aadaileh <ahmed.adaileh@gmail.com>
2018-01-12 00:09:03 +0100

    add database connection

--- src/main/java/abcbankinggroup/ApiController.java
+++ src/main/java/abcbankinggroup/ApiController.java
@@ -1,22 +1,37 @@
 package abcbankinggroup;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 @RestController
 public class ApiController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApiController.class);
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
+    @Autowired
+    private DataSource dataSource;
 
     /**
      * Add some comments
@@ -48,6 +63,55 @@ public class ApiController {
         return returnedUser;
     }
 
+
+    /**
+     * Add some comments
+     */
+    @ApiOperation("Create a new Lead")
+    @RequestMapping(value = "/db",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            method = RequestMethod.POST)
+    @ResponseBody
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 406, message = "Not Acceptable. Validation of data failed.") })
+    public void db() {
+
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getTimestamp("tick"));
+            }
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            int x=0;
+
+        }
+    }
+
+
+        @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            config.setUsername("b9579c6ae9cba0");
+            config.setPassword("89a88141");
+            return new HikariDataSource(config);
+        }
+    }
     @ExceptionHandler
     void handleIllegalArgumentException(
             IllegalArgumentException e,
---
+++ src/main/java/abcbankinggroup/configuration/DataBaseConfiguration.java
@@ -0,0 +1,17 @@
+package abcbankinggroup.configuration;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "params.datasource")
+public class DataBaseConfiguration extends HikariConfig {
+
+//    @Bean
+//    public DataSource dataSource() throws SQLException {
+//        return new HikariDataSource(this);
+//    }
+
+}
